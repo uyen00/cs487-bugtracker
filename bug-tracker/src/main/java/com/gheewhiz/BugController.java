27d0cb7bffaf81f0fe29fe76192742215281b1f0
vguharoy <vguharoy@34f4a2a8-1f41-11de-b650-e715bd6d7cf1>
2009-05-14 00:01:22 +0000

    v

--- bug-tracker/src/main/java/com/gheewhiz/BugController.java
+++ bug-tracker/src/main/java/com/gheewhiz/BugController.java
@@ -18,45 +18,62 @@ public class BugController {
 	public void setBugTrackerService(BugTrackerService bugTrackerService) {
 		this.bugTrackerService = bugTrackerService;
 	}
-	
+
 	@RequestMapping(value = { "/bugs.html" })
-	public String handleBugs(HttpSession session, 
-			@RequestParam("productId")Integer productId, Model model) {
+	public String handleBugs(HttpSession session,
+			@RequestParam("productId") Integer productId, Model model) {
 		model.addAttribute("product", bugTrackerService.getProduct(productId));
 		model.addAttribute("bugs", bugTrackerService.getBugs(productId));
 		return "bugs";
 	}
-	
+
 	@RequestMapping(value = { "/bug.html" })
-	public String handleBug(HttpSession session, @RequestParam("bugId")Integer bugId, Model model) {
+	public String handleBug(HttpSession session,
+			@RequestParam("bugId") Integer bugId, Model model) {
 		model.addAttribute("bugs", bugTrackerService.getBug(bugId));
 		return "bug";
 	}
-	
+
 	@RequestMapping(value = { "/create-bug.html" })
-	public String handleCreateBug(HttpSession session, @RequestParam("productId")Integer productId, @RequestParam("state")String state, @RequestParam("resolution")String resolution, @RequestParam("shortdesc")String shortdesc, @RequestParam("steps")String steps, @RequestParam("comments")Set<Comment> comments, Model model) {
-		model.addAttribute("create-bug", bugTrackerService.createBug(productId, state, resolution, shortdesc, steps, comments));
+	public String handleCreateBug(HttpSession session,
+			@RequestParam("productId") Integer productId,
+			@RequestParam("state") String state,
+			@RequestParam("resolution") String resolution,
+			@RequestParam("shortdesc") String shortdesc,
+			@RequestParam("steps") String steps,
+			@RequestParam("comments") Set<Comment> comments, Model model) {
+		model.addAttribute("create-bug", bugTrackerService.createBug(productId,
+				state, resolution, shortdesc, steps, comments));
 		return "create-bug";
 	}
-	
+
 	@RequestMapping(value = { "/create-comment.html" })
-	public String handleCreateComment(HttpSession session, @RequestParam("comment")String comment, @RequestParam("bugId")Integer bugId, Model model) {
-		model.addAttribute("create-comment", bugTrackerService.createComment(comment, bugId, getSessionAccount(session)));
+	public String handleCreateComment(HttpSession session,
+			@RequestParam("comment") String comment,
+			@RequestParam("bugId") Integer bugId, Model model) {
+		model.addAttribute("create-comment", bugTrackerService.createComment(
+				comment, bugId, getSessionAccount(session)));
 		return "create-comment";
 	}
-	
+
 	@RequestMapping(value = { "/products.html" })
 	public String handleProducts(HttpSession session, Model model) {
 		model.addAttribute("products", bugTrackerService.getProducts());
 		return "products";
 	}
-	
+
 	@RequestMapping(value = { "/create-product.html" })
-	public String handleCreateProduct(HttpSession session, @RequestParam("name")String name, @RequestParam("version")String version, @RequestParam("manager")Account manager, @RequestParam("qa")Set<Account> qa, @RequestParam("developers")Set<Account> developers, Model model) {
-		model.addAttribute("create-product", bugTrackerService.createProductCategory(name, version, manager, qa, developers));
+	public String handleCreateProduct(HttpSession session,
+			@RequestParam("name") String name,
+			@RequestParam("version") String version,
+			@RequestParam("manager") Account manager,
+			@RequestParam("qa") Set<Account> qa,
+			@RequestParam("developers") Set<Account> developers, Model model) {
+		model.addAttribute("create-product", bugTrackerService
+				.createProductCategory(name, version, manager, qa, developers));
 		return "create-product";
 	}
-	
+
 	private Account getSessionAccount(HttpSession session) {
 		return (Account) session.getAttribute("account");
 	}
