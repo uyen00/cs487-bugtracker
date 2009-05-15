f969113c436e82b773db6a58100039bc1bd04a68
moonkev <moonkev@34f4a2a8-1f41-11de-b650-e715bd6d7cf1>
2009-05-15 19:32:21 +0000

    dfg

--- trunk/bug-tracker/src/test/java/com/gheewhiz/test/BugTrackerDaoTest.java
+++ trunk/bug-tracker/src/test/java/com/gheewhiz/test/BugTrackerDaoTest.java
@@ -2,7 +2,6 @@ package com.gheewhiz.test;
 
 import java.util.Collections;
 import java.util.Date;
-import java.util.Set;
 
 import javax.annotation.Resource;
 
@@ -10,13 +9,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.springframework.transaction.annotation.Transactional;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.gheewhiz.Account;
 import com.gheewhiz.Bug;
 import com.gheewhiz.BugTrackerDao;
-import com.gheewhiz.Comment;
 import com.gheewhiz.Entitlement;
 import com.gheewhiz.ProductCategory;
 
@@ -26,11 +23,6 @@ import com.gheewhiz.ProductCategory;
 public class BugTrackerDaoTest extends
 		AbstractTransactionalTestNGSpringContextTests {
 
-        private Bug bugStub;
-        private ProductCategory productStub;
-        private Account accountStub;
-        private Comment commentStub;
-        
 	@Resource
 	BugTrackerDao bugTrackerDao;
 
@@ -38,17 +30,6 @@ public class BugTrackerDaoTest extends
 		this.bugTrackerDao = bugTrackerDao;
 	}
 
-        @BeforeClass(alwaysRun=true)
-        public void init() {
-            if(accountStub == null)
-            accountStub = accountManagerStub();
-            if(productStub == null)
-            productStub = productCategoryStub(accountStub);
-            if(bugStub == null)
-            bugStub = bugStub(productStub);
-        }
-        
-        
 	@Test(groups = { "basic" })
 	@Rollback(true)
 	public void testAccountDaoOperations() {
@@ -67,61 +48,35 @@ public class BugTrackerDaoTest extends
 		bugTrackerDao.removeAccount(dbAccount);
 		assert bugTrackerDao.getAccount(accountId) == null;
 	}
-        
-        @Test(groups = { "comment" })
-        @Rollback(true)
-        public void testCommentDaoOperations() {        
-            Comment comment = new Comment();
-            comment.setBugId(bugStub.getBugId());
-            comment.setComment("comment test");
-            comment.setCommenter(accountStub);
-            Comment dbComment = bugTrackerDao.createComment(comment);
-            Integer commentId = dbComment.getCommentId();
-            assert commentId != null;
-            assert dbComment.getComment().equals("comment test");
-            comment.setComment("comment test2");
-            bugTrackerDao.updateComment(comment);
-            dbComment = getCommentById(bugTrackerDao.getComments(bugStub.getBugId()), dbComment.getCommentId());
-        }
-        
-        
-        private Comment getCommentById(Set<Comment> comments, Integer commentId) {
-            for(Comment comment : comments) {
-                if(commentId.equals(comment.getCommentId())) {
-                    return comment;
-                }
-            }
-            return null;
-        }
-        
-        public Bug bugStub(ProductCategory product) {
-            Bug bug = new Bug();
-            bug.setOpened(new Date());
-            bug.setShortdesc("DESC");
-            bug.setResolution("IN PROGRESS");
-            bug.setState("OPEN");
-            bug.setSteps("Do this");
-            bug.setProductId(product.getProductCategoryId());
-            return bugTrackerDao.createBug(bug);
-        }
-        
-        public ProductCategory productCategoryStub(Account manager) {
-            ProductCategory product = new ProductCategory();
-            product.setManager(manager);
-            product.setName("Good Product");
-            product.setVersion("1.0");
-            return bugTrackerDao.createProductCategory(product);
-        }
-        
-        public Account accountManagerStub() {
-                Account account = bugTrackerDao.getAccountByScreenName("BigBoss");
-                if(account != null) {
-                    return account;
-                }
-            	account = new Account();
+
+	public Bug bugStub(ProductCategory product) {
+		Bug bug = new Bug();
+		bug.setOpened(new Date());
+		bug.setShortdesc("DESC");
+		bug.setResolution("IN PROGRESS");
+		bug.setState("OPEN");
+		bug.setSteps("Do this");
+		bug.setProductId(product.getProductCategoryId());
+		return bugTrackerDao.createBug(bug);
+	}
+
+	public ProductCategory productCategoryStub(Account manager) {
+		ProductCategory product = new ProductCategory();
+		product.setManager(manager);
+		product.setName("Good Product");
+		product.setVersion("1.0");
+		return bugTrackerDao.createProductCategory(product);
+	}
+
+	public Account accountManagerStub() {
+		Account account = bugTrackerDao.getAccountByScreenName("BigBoss");
+		if (account != null) {
+			return account;
+		}
+		account = new Account();
 		account.setPassword("123");
 		account.setScreenName("BigBoss");
-                account.setEntitlements(Collections.singleton(Entitlement.MANAGER));
+		account.setEntitlements(Collections.singleton(Entitlement.MANAGER));
 		return bugTrackerDao.createAccount(account);
-        }
+	}
 }
